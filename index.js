a5387a12648d9ea7d5397f3d552f8afb3be2d6b3
elmehdi05tabi <tabimehdi0@gmail.com>
2026-03-08 02:31:31 +0000

    livre realod

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose') 
-const myData = require('./models/myDataSchema') 
+const myData = require('./modules/myDataSchema') 
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
   myData.find()
   .then((result)=>res.render('home',{"data":result}))
   .catch((err)=>console.log(err))  
+}) ; 
+app.get('/create',(req,res)=>{
+  res.render('create') ; 
 })
+app.use(express.static('public')) ; 
 mongoose.connect('mongodb+srv://elmehdi:15HR1WyAmlClAqXk@cluster0.thsi7k7.mongodb.net/?appName=Cluster0')
 .then(()=>app.listen(port, () => {  
   console.log(`http://localhost:${port}/`)
@@ -20,3 +24,18 @@ app.post('/',(req,res)=>{
   .then(()=>res.redirect('/'))
   .catch((err)=>console.log(err))
 })
+ 
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+ 
+ 
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+ 
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
